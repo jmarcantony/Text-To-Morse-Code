6721ba417ca7735148f0a3d4e9df754b56801dfa
ninjahacker123 <josephmarc2610@gmail.com>
2021-08-01 07:11:41 +0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from banner import banner
+
+morse_code = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-', ' ': ' / '
+}
+
+def convert_to_morse(text):
+	split_chars = [char.upper() for char in text]
+	converted_chars = [morse_code[char] for char in split_chars]
+	converted_text = ''.join(converted_chars)
+
+	return converted_text
+
+
+print(banner)
+
+text = input("[*] Enter text to be converted: ")
+print(f"[+] Morse code for inputed text: {convert_to_morse(text)}")
+
